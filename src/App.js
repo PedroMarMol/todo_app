ffe22fa1a37a2ef1a3d3636aec0335c7cc1185a8
Pedro Martos <martosmoleropedro@gmail.com>
2023-02-12 15:11:26 +0100

    Add Todo component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react"
+import { AiOutlinePlusCircle } from "react-icons/ai"
+import Todo from "./components/Todo"
 
 function App() {
+  const [todos, setTodos] = useState(['Use reusable code', 'Apply SOLID Principles']) 
+
   return (
     <div>
-      test
+      <div>
+        <h3>Todo App</h3>
+        <form>
+          <input type="text" placeholder="Add Todo" />
+          <button><AiOutlinePlusCircle size={30}/></button>
+        </form>
+        <ul>
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo} />
+          ))}
+        </ul>
+        <p>You have x-number todos</p>
+      </div>
     </div>
   );
 }
