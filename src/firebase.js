7f2ae1639c56daf5ea1121f3120d11b2431da529
PedroMarMol <martosmoleropedro@gmail.com>
2023-02-15 13:33:46 +0100

    Install Firebase Authentication

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDmbk-s3en28jRB3MrKDLG5-rD34HvQ5uE",
@@ -12,4 +13,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 export const db = getFirestore(app);
