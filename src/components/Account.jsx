9e9eeed0d3da9e67cd147d094b90ef6062753b66
PedroMarMol <martosmoleropedro@gmail.com>
2023-02-15 13:34:48 +0100

    Create Account Route boilerplate

---
+++ src/components/Account.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Account = (props) => {
+  return (
+    <div className="">
+        <h1 className="text-2xl font-bold py-4">
+            Account
+        </h1>
+        <p>User Email:</p>
+
+        <button className="border px-6 py-2 my-4">
+            Logout
+        </button>
+    </div>
+  )
+};
+
+export default Account
