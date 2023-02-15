c0618e74914a686bfc33ad0c7995546327226167
PedroMarMol <martosmoleropedro@gmail.com>
2023-02-15 13:34:16 +0100

    Add Router setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const el = document.getElementById('root');
+const root = createRoot(el);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );  
