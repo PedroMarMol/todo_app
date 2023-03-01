a152fa6b0837a60efabc4b80b47fe823917646a8
PedroMarMol <martosmoleropedro@gmail.com>
2023-03-01 13:49:22 +0100

    Remove List and make it a component, set Routing (with Protected Routes)

--- src/App.js
+++ src/App.js
@@ -1,114 +1,70 @@
-import React, { useState, useEffect } from "react"
-import Signin from "./components/Signin"
-import Signup from "./components/Signup"
-import Account from "./components/Account" 
-import { Route, Routes } from "react-router-dom"
-import { AiOutlinePlusCircle } from "react-icons/ai"
-import Todo from "./components/Todo"
-import { db } from './firebase'
-import { 
-  query, 
-  collection, 
-  onSnapshot, 
-  doc, 
-  updateDoc, 
-  addDoc, 
-  deleteDoc, } from 'firebase/firestore'
-
-const style = {
-  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
-  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
-  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
-  form: `flex justify-between`,
-  input: `border p-2 w-full text-xl`,
-  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
-  count: `text-center p-2`,
-}
+import React from 'react'
+import LogIn from './components/LogIn'
+import Signup from './components/SignUp'
+import Account from './components/Account' 
+import { Route, Routes} from 'react-router-dom'
+import { AuthContextProvider } from './context/AuthContext'
+import PrivateRoute from './components/PrivateRoute'
+import PublicRoute from './components/PublicRoute'
+import List from './components/List'
+import NavBar from './components/NavBar'
+import Homepage from './components/Homepage'
 
 function App() {
-  let defaultStatus = false;
-  const [todos, setTodos] = useState([]);
-  const [newItem, setNewItem] = useState('');
-
-  const addTodo = async (event) => {
-    event.preventDefault(event)
-    if(newItem === '') { // change error handler
-      alert('Enter valid string')
-      return
+  const style = {
+    bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+    container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`
     }
-    await addDoc(collection(db, 'todos'), {
-      text: newItem,
-        status: defaultStatus
-    })
-    setNewItem('')
-  };
-  
-  const deleteTodo = async (id) => {
-    await deleteDoc(doc(db, 'todos', id))
-  }
-
-  // Read item from Firebase
-  useEffect(() => {
-    const q = query(collection(db, 'todos'))
-    const unsubscribe = onSnapshot(q, (querySnapshot) => {
-      let todosArr = []
-      querySnapshot.forEach((item) => {
-        todosArr.push({...item.data(), id: item.id})
-      });
-      setTodos(todosArr)
-    })
-    return () => unsubscribe()
-  }, [])
-
-  // Update item from Firebase
-  const toggleStatus = async (todo) => {
-    await updateDoc(doc(db, 'todos', todo.id), {
-      status: !todo.status
-    })
-  }
-
   return (
     <div className={style.bg}>
+        <NavBar />
       <div className={style.container}>
-        <Routes>
-          <Route path='/signin' element={<Signin />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/account' element={<Account />} />
-        </Routes>
-        <h3 className={style.heading}>
-          Todo App
-        </h3>
-        <form 
-          onSubmit={addTodo} 
-          className={style.form}
-        >
-          <input
-            value={newItem} 
-            className={style.input}
-            onChange={(event) => setNewItem(event.target.value)} 
-            type="text" 
-            placeholder="Add Todo" 
-          />
-          <button className={style.button}>
-            <AiOutlinePlusCircle size={30}/>
-          </button>
-        </form>
-        <ul>
-          {todos.map((todo, index) => (
-            <Todo 
-              key={index} 
-              todo={todo} 
-              toggleStatus={toggleStatus} 
-              deleteTodo={deleteTodo} 
-            />
-          ))}
-        </ul>
-        {todos.length < 1 ? 
-          null : 
-          <p className={style.count}>
-            {`You have ${todos.length} todos`}
-          </p>
-        }
+        <AuthContextProvider>
+            <Routes>
+              <Route
+                exact path='/'
+                element={
+                  <Homepage />
+                }
+              />
+              <Route 
+                path='/login' 
+                element={
+                  <PublicRoute>
+                    <LogIn />
+                  </PublicRoute>}
+              />
+              <Route 
+                path='/signup' 
+                element={
+                  <PublicRoute>
+                    <Signup />
+                  </PublicRoute>}
+              />
+              <Route 
+                path='/account'
+                element={
+                  <PrivateRoute>
+                    <Account />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path='/list'
+                element={
+                  <PrivateRoute>
+                    <List />
+                  </PrivateRoute>}
+              />
+              <Route
+                path='*'
+                element={
+                  <PrivateRoute>
+                    <Homepage />
+                  </PrivateRoute>}
+              />
+            </Routes>  
+        </AuthContextProvider>
       </div>
     </div>
   );
