39f273147927b11c519e1f529736fc9432e57221
PedroMarMol <martosmoleropedro@gmail.com>
2023-03-02 10:48:22 +0100

    Create List component that maps over the items showing every ListItem component and reads/adds/removes items from database

---
+++ src/components/List.jsx
@@ -0,0 +1,128 @@
+import React, {useState, useEffect} from "react"
+import { AiOutlinePlusCircle } from "react-icons/ai"
+import { db } from '../firebase'
+import { 
+  query, 
+  collection, 
+  onSnapshot, 
+  doc, 
+  updateDoc, 
+  addDoc, 
+  deleteDoc, } from 'firebase/firestore'
+import ListItem from "./ListItem"
+import { UserAuth } from "../context/AuthContext"
+import { ImSpinner2 } from "react-icons/im"
+
+const List = (props) => {
+  const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+  count: `text-center p-2`,
+  }
+
+  let defaultStatus = false;
+  const [items, setItems] = useState([])
+  const [newItem, setNewItem] = useState('')
+  const { user, fetchingSession } = UserAuth()
+  let userId = user.uid
+
+  // const [user, setUser] = useState('')
+  // async function fetchUser() {
+  //   const { user } = await UserAuth()
+  // }
+  
+  async function fetchData() {
+    // if (fetchingSession) {
+    //   return <ImSpinner2 size={30}/>
+    // } else {
+      const q = await query(collection(db, userId))
+      onSnapshot(q, (querySnapshot) => {
+        let itemsArr = []
+        querySnapshot.forEach((item) => {
+          itemsArr.push({...item.data(), id: item.id})
+        });
+        setItems(itemsArr)
+      })
+    // }
+  }
+  
+  useEffect(() => {
+    if (fetchingSession) {
+        return <ImSpinner2 size={30}/>
+    } else fetchData()
+  }, [])
+      
+  // Add Item to Firebase
+  async function addItem(event) {
+    event.preventDefault(event)
+    if(newItem === '') { // change error handler
+      alert('Enter valid string')
+      return
+    }
+    await addDoc(collection(db, userId), {
+      text: newItem,
+      status: defaultStatus
+    })
+    setNewItem('')
+  };
+
+  // Delete Item from Firebase
+  async function deleteItem(id) {
+    await deleteDoc(doc(db, userId, id))
+  }
+
+
+
+  // Update item from Firebase
+  async function toggleStatus(item) {
+    await updateDoc(doc(db, userId, item.id), {
+      status: !item.status
+    })
+  }
+
+
+  return (
+    <div>
+      <h3 className={style.heading}>
+        Todo App
+      </h3>
+      <form 
+        onSubmit={addItem} 
+        className={style.form}
+      >
+        <input
+          value={newItem} 
+          className={style.input}
+          onChange={(event) => setNewItem(event.target.value)} 
+          type="text" 
+          placeholder="Add Todo" 
+        />
+        <button className={style.button}>
+          <AiOutlinePlusCircle size={30}/>
+        </button>
+      </form>
+      <ul>
+        {items.map((item, index) => (
+          <ListItem 
+            key={index} 
+            item={item} 
+            toggleStatus={toggleStatus} 
+            deleteItem={deleteItem} 
+          />
+        ))}
+      </ul>
+      {items.length < 1 ? 
+        null : 
+        <p className={style.count}>
+          {`You have ${items.length} todos`}
+        </p>
+      }
+    </div>
+  )
+}
+
+export default List
