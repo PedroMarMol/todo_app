9c4c08f3626f7a58870c6e58cd9b2f89e869591a
PedroMarMol <martosmoleropedro@gmail.com>
2023-03-09 18:50:15 +0100

    Notify the user that he is not logged in if he tries to access the List component. Change spinner icon and put a react spinner instead.

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,27 +1,35 @@
 import { React, useEffect, useState } from 'react'
 import { Navigate } from 'react-router-dom'
 import { useUserAuth } from '../context/AuthContext'
-import { ImSpinner2 } from 'react-icons/im'
+import { toast } from "react-toastify"
+import { PulseLoader } from 'react-spinners'
 
 const PrivateRoute = ({ children }) => {
   const { user } = useUserAuth()
   const [isLoading, setIsLoading] = useState(true)
+  const [alertShown, setAlertShown] = useState(false)
 
   useEffect(() => {
     // Delay showing the loading icon for 2 seconds
     const delayLoading = setTimeout(() => {
       setIsLoading(false)
-    }, 200)
+    }, 500)
     // Clean up the setTimeout when the component unmounts or the state changes
     return () => clearTimeout(delayLoading)
   }, [])
 
+  useEffect(() => {
+    // show the alert only if the user is not defined and the loading has finished
+    if (!user && !isLoading && !alertShown) {
+      toast.warn('You are not logged in!')
+      setAlertShown(true)
+    }
+  }, [user, isLoading, alertShown])
+
   return (
-    <div>
-      {isLoading ? (<ImSpinner2 size={30}/>) 
-      : (
-       user ? children : <Navigate to='/signup' />
-      )}
+    <div >
+      {isLoading ? (<div className='flex justify-center'><PulseLoader color="#2F80ED" /></div>)
+      : ( user ? children : <Navigate to='/signup' />)}
     </div>
   )
 
