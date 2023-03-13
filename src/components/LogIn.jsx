6f3bddffb18cfc6b5ea81a04213355a9dca6a832
PedroMarMol <martosmoleropedro@gmail.com>
2023-03-13 13:13:03 +0100

    Fix label opacity styling bug and error handling

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { useState } from 'react'
+import { React, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useUserAuth } from '../context/AuthContext'
 import { HiOutlineMail } from 'react-icons/hi'
 import { BiLockAlt } from 'react-icons/bi'
-import { toast } from "react-toastify"
+import { toast } from 'react-toastify'
+import useInputVisibility from '../hooks/useInputVisibility';
+import handleFirebaseError from '../utils/handleFirebaseError'
 
 const LogIn = (props) => {
   const style = {
@@ -14,62 +15,50 @@ const LogIn = (props) => {
 		form: `grid place-items-center gap-4`,
 		formContainer: `flex items-baseline`,
 		inputContainer: `relative mb-3 xl:w-96 data-te-input-wrapper-init`,
-		input: `w-241 peer min-h-[auto] rounded border-0 bg-transparent py-[0.32rem]
-			px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100
-			data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 
-			dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0`,
-		label: `pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0]
-			truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 
-			ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary 
-			peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] 
-			motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-neutral-400`,
 		button: `text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl dark font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2`
 	}
 	const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [error, setError] = useState('')
 	const navigate = useNavigate()
 	const { logIn } = useUserAuth()
+  const isEmailFilled = useInputVisibility('inputEmail')
+  const isPasswordFilled = useInputVisibility('inputPassword')
 
   function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
   }
 
-	const useHandleSubmit = async (event) => {
-		event.preventDefault()
-		setError('')
-		try {
-      toast.info('You are logging in!')
-      await delay(800)
+	const handleSubmit = async (event) => {
+    event.preventDefault()
+    toast.info('You are logging in!')
+    try {
+      await delay(1000)
 			await logIn(email, password)
 			navigate('/list')
-		} catch(event) {
-			setError(event.message)
-			toast.warn(event.message)
-		}
+		} catch(error) {
+			handleFirebaseError(error)
+    }
 	}
 
-  return (
+return (
     <div className={style.bg}>
       <div className={style.container}>
         <img src='https://flowbite.com/docs/images/logo.svg' alt='deja vu logo'/>
         <h1 className={style.header}>Welcome back</h1>
-        <form className={style.form} onSubmit={useHandleSubmit}>
+        <form className={style.form} onSubmit={handleSubmit}>
           <div className={style.formContainer}>
             <HiOutlineMail size={17}/>
             <div className={style.inputContainer}>
               <input 
                 onChange={(event) => setEmail(event.target.value)} 
-                className={style.input}
                 type='email'
-                id='email'
-                placeholder='E-mail address'
+                className='peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 &:not([data-te-input-placeholder-active]):placeholder:opacity-0'
+                id='inputEmail'
               />
               <label 
-                className={style.label}
-                htmlFor='email' 
-              >
-                Email
+                className={`pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200 ${isEmailFilled ? 'hidden' : ''}`}
+                htmlFor='inputEmail' 
+              > Email
               </label>
             </div>
           </div>
@@ -78,22 +67,18 @@ const LogIn = (props) => {
             <div className={style.inputContainer}>
               <input 
                 onChange={(event) => setPassword(event.target.value)} 
-                className={style.input}
                 type='password'
-                id='password'
-                placeholder='Password'
+                className='peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 &:not([data-te-input-placeholder-active]):placeholder:opacity-0'
+                id='inputPassword'
               />
               <label 
-                className={style.label}
-                htmlFor='password' 
-              >
-                Password
+                htmlFor='inputPassword' 
+                className={`pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200 ${isPasswordFilled ? 'hidden' : ''}`}
+              > Password
               </label>
             </div>
           </div>
-          <button className={style.button}>
-          	Log in to your account
-          </button>
+          <button className={style.button}> Log in to your account</button>
         </form>
         <p className='py-2'>
           Just getting started? <Link to='/signup' className='underline'>Create an account</Link>
@@ -101,6 +86,5 @@ const LogIn = (props) => {
       </div>
     </div>
   )
-};
-
+}
 export default LogIn;
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -4,6 +4,8 @@ import { useUserAuth } from '../context/AuthContext'
 import { HiOutlineMail } from 'react-icons/hi'
 import { BiLockAlt } from 'react-icons/bi'
 import { toast } from 'react-toastify'
+import useInputVisibility from '../hooks/useInputVisibility'
+import handleFirebaseError from '../utils/handleFirebaseError'
 
 export default function SignUp() {
 	const style = {
@@ -13,60 +15,49 @@ export default function SignUp() {
 		form: `grid place-items-center gap-4`,
 		formContainer: `flex items-baseline`,
 		inputContainer: `relative mb-3 xl:w-96 data-te-input-wrapper-init`,
-		input: `w-241 peer min-h-[auto] rounded border-0 bg-transparent py-[0.32rem]
-			px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100
-			data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 
-			dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0`,
-		label: `pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0]
-			truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 
-			ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary 
-			peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] 
-			motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-neutral-400`,
 		button: `text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl dark font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2`
 	}
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [error, setError] = useState('')
 	const navigate = useNavigate()
 	const { createUser } = useUserAuth()
+  const isEmailFilled = useInputVisibility('inputEmail')
+  const isPasswordFilled = useInputVisibility('inputPassword')
 
   function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms))
   }
 
-	const HandleSubmit = async (event) => {
+	const handleSubmit = async (event) => {
 		event.preventDefault()
-		setError('')
-		try{
-			toast.info('You just signed up!')
-      await delay(800)
+		toast.info('You just signed up!')
+		try {
+      await delay(1000)
 			await createUser(email, password)
 			navigate('/list')
-		}	catch (event) {
-			setError(event.message)
-			toast.warn(event.message)
+		}	catch (error) {
+			handleFirebaseError(error)
 		}
-	}
+}
 
   return (
   	<div className={style.bg}>
   	  <div className={style.container}>
 				<img src='https://flowbite.com/docs/images/logo.svg' alt='deja vu logo'/>
         <h1 className={style.header}>Welcome to Deja Vu!</h1>
-  	    <form className={style.form} onSubmit={HandleSubmit}>
+  	    <form className={style.form} onSubmit={handleSubmit}>
 					<div className={style.formContainer}>
 						<HiOutlineMail size={17}/>
 						<div className={style.inputContainer}>
 							<input 
 								onChange={(event) => setEmail(event.target.value)} 
-								className={style.input}
 								type='email'
-								id='email'
-								placeholder='E-mail address'
+								className='peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 &:not([data-te-input-placeholder-active]):placeholder:opacity-0'
+								id='inputEmail'
 							/>
 							<label 
-								className={style.label} 
-								htmlFor='email' 
+								className={`pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200 ${isEmailFilled ? 'hidden' : ''}`}
+								htmlFor='inputEmail' 
 							>
 								Email
 							</label>
@@ -77,16 +68,14 @@ export default function SignUp() {
 						<div className={style.inputContainer}>
 							<input 
 								onChange={(event) => setPassword(event.target.value)} 
-								className={style.input}
 								type='password'
-								id='password'
-								placeholder='Password'
+								className='peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 &:not([data-te-input-placeholder-active]):placeholder:opacity-0'
+								id='inputPassword'
 							/>
 							<label 
-								className={style.label} 
-								htmlFor='password' 
-							>
-								Password
+								className={`pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200 ${isPasswordFilled ? 'hidden' : ''}`}
+								htmlFor='inputPassword' 
+							> Password
 							</label>
 						</div>
 					</div>
