b66d69db69eb72a3a096bb694301480299b72c25
PedroMarMol <martosmoleropedro@gmail.com>
2023-03-13 10:38:18 +0100

    Add styling and optimize code

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -4,6 +4,13 @@ import { toast } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
 
 const Account = (props) => {
+  const style = {
+    container: `text-center`,
+    header: `text-3xl font-bold text-center text-gray-800 p-2`,
+    info: `mb-2`,
+    subheader: `text-xl`,
+    button: `text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl dark font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2`,
+  }
   const { user, logOut } = useUserAuth()
   const userId = user.uid
   const userEmail= user.email
@@ -23,15 +30,17 @@ const Account = (props) => {
   }
   
   return (
-    <div className="">
-      <h1 className="text-2xl font-bold py-4">
-          Account
-      </h1>
-      <p>User Email: {userEmail}</p>
-      <p>User ID: {userId}</p>
-      <button onClick={handleLogout} className="border px-6 py-2 my-4">
-          Logout
-      </button>
+    <div className={style.container}>
+      <h1 className={style.header}>Account Details</h1>
+      <div className={style.info}>
+        <p className={style.subheader}>User Email</p>
+        {userEmail}
+      </div>
+      <div>
+        <button onClick={handleLogout} className={style.button}>
+            Logout
+        </button>
+      </div>
     </div>
   )
 };
