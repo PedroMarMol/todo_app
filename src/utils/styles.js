a95e4115daadd6f656a0a5fd9d1bd7690dd3d82f
PedroMarMol <martosmoleropedro@gmail.com>
2023-03-13 13:22:41 +0100

    Made reusable styling for SignUp/LogIn components

---
+++ src/utils/styles.js
@@ -0,0 +1,9 @@
+export const style = {
+  bg: `max-w-[700px] mx-auto my-4`,
+  container: `grid place-items-center gap-4`,
+  header: `text-2xl font-bold py-2`,
+  form: `grid place-items-center gap-4`,
+  formContainer: `flex items-baseline`,
+  inputContainer: `relative mb-3 xl:w-96 data-te-input-wrapper-init`,
+  button: `text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl dark font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2`
+}
