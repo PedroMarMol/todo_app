2188fd67ad3861408a9499319491b5df99dcc731
PedroMarMol <martosmoleropedro@gmail.com>
2023-03-13 14:23:39 +0100

    Make Account styling reusable

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,16 +1,10 @@
 import React from "react"
 import { useUserAuth } from "../context/AuthContext"
 import { toast } from "react-toastify"
-import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css'
+import { style } from "../utils/styles"
 
 const Account = (props) => {
-  const style = {
-    container: `text-center`,
-    header: `text-3xl font-bold text-center text-gray-800 p-2`,
-    info: `mb-2`,
-    subheader: `text-xl`,
-    button: `text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl dark font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2`,
-  }
   const { user, logOut } = useUserAuth()
   const userEmail= user.email
   const userId = user.uid
@@ -30,7 +24,7 @@ const Account = (props) => {
   }
   
   return (
-    <div className={style.container}>
+    <div className="text-center">
       <h1 className={style.header}>Account Details</h1>
       <div className={style.info}>
         <p className={style.subheader}>User Email</p>
--- src/utils/styles.js
+++ src/utils/styles.js
@@ -4,6 +4,8 @@ export const style = {
   header: `text-2xl font-bold py-2`,
   form: `grid place-items-center gap-4`,
   formContainer: `flex items-baseline`,
+  info: `mb-2`,
+  subheader: `text-xl font-semibold`,
   inputContainer: `relative mb-3 xl:w-96 data-te-input-wrapper-init`,
   button: `text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl dark font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2`
 }
